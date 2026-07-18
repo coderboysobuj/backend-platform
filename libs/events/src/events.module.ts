130377adc9289790b513d0a0d1506d142af86ad7
Mohammad Sobuj <coderboysobuj@gmail.com>
2026-07-18 21:36:23 +0600

    lib: events

---
+++ libs/events/src/events.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { DomainEventEmitter } from './domain-events';
+
+@Module({
+  imports: [
+    EventEmitterModule.forRoot({
+      wildcard: false,
+      delimiter: '.',
+      maxListeners: 20,
+      verboseMemoryLeak: true,
+      ignoreErrors: false,
+    }),
+  ],
+  providers: [DomainEventEmitter],
+  exports: [DomainEventEmitter, EventEmitterModule],
+})
+export class DomainEventsModule {}
+
---
+++ libs/events/src/index.ts
@@ -0,0 +1,16 @@
+export { DomainEventsModule } from './events.module';
+export {
+  DomainEventEmitter,
+  BaseDomainEvent,
+  UserRegisteredEvent,
+  UserLoggedInEvent,
+  UserEmailVerifiedEvent,
+  UserPasswordChangedEvent,
+  UserDeactivatedEvent,
+  OrgCreatedEvent,
+  OrgMemberJoinedEvent,
+  OrgMemberRemovedEvent,
+  SubscriptionActivatedEvent,
+  SubscriptionCanceledEvent,
+  NotificationCreatedEvent,
+} from './domain-events';
